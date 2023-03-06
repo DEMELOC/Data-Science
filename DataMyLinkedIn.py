93cce2ecd9fd50296ebdbbe74383c5f73901cc03
Cleiton Melo <97135043+DestinyEndless@users.noreply.github.com>
2023-03-06 16:16:41 -0300

    Setting Indexer
    Setting the ID function as indexer

--- DataMyLinkedIn.py
+++ DataMyLinkedIn.py
@@ -21,11 +21,18 @@ input("Press Enter to continue")
 
 # Adding random ID column
 df.reset_index(inplace=True)
-df.rename(columns={'index':'id'}, inplace=True)
+df.rename(columns={'index':'Id'}, inplace=True)
 df.info()
 input("Press Enter to continue")
 
-# Fuction ID = 8 characters
+# Fuction ID = 8 charecters
 def get_id():
-  id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
-  return id
+  Id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
+  return Id
+
+# Printing all columns of the dataframe
+print(df.columns.tolist())
+
+# Setting ID with get_id function
+for r in range(df.shape[0]):
+  df[get_id].iloc._setitem_with_indexer(r, get_id())
